a13f5f2be6320dee156c3832bc600d41b60f1b9c
DV <58750067+v-david@users.noreply.github.com>
2025-07-17 15:36:50 -0400

    target10

--- main.tf
+++ main.tf
@@ -36,13 +36,13 @@ module "blog_vpc" {
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "9.0.1"
-  
+  target_group_arns   = [module.blog_alb.target_groups.arn]
+
   name     = "blog"
   min_size = 1
   max_size = 2
-
-  #target_group_arns   = [module.blog_alb.target_groups.arn]
-  vpc_zone_identifier = modules.blog_vpc.public_subnets  
+  
+  vpc_zone_identifier = module.blog_vpc.public_subnets  
   security_groups     = [module.blog_sg.security_group_id]
 
   image_id      = data.aws_ami.app_ami.id
