cf8dd871afa9b4af408bf159b255ee3692bb4ed3
DV <58750067+v-david@users.noreply.github.com>
2025-07-17 16:07:36 -0400

    new9

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ module "blog_autoscaling" {
   max_size            = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
+  target_group_arns   = module.blog_alb.target_group["ex-instance"].arn
   security_groups     = [module.blog_sg.security_group_id]
 
   instance_type       = var.instance_type
