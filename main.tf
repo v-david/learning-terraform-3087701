899acd00d94d1402aeb09e2808915a4bc9383f89
DV <58750067+v-david@users.noreply.github.com>
2025-07-17 12:41:46 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,8 +17,8 @@ data "aws_ami" "app_ami" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.micro"
-  region = "us-east-2c"
+  region = "us-east-2"
   tags = {
-    Name = "HelloWorld"
+    Name = "HelloWorld2"
   }
 }
