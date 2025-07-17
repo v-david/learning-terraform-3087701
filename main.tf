04faaad4758dd5d395963bbb6c389386d64611d6
DV <58750067+v-david@users.noreply.github.com>
2025-07-17 15:56:59 -0400

    new4

--- main.tf
+++ main.tf
@@ -38,12 +38,13 @@ module "blog_autoscaling" {
   version = "9.0.1"
 
   name = "blog"
-
   min_size            = 1
   max_size            = 2
+
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
+  
   instance_type       = var.instance_type
   image_id            = data.aws_ami.app_ami.id
 }
@@ -66,6 +67,12 @@ module "blog_alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+      }
     }
   ]
 
