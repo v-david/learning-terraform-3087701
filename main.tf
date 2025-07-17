be28f647a4f1b7bf2884a078f8878cfb240660fd
DV <58750067+v-david@users.noreply.github.com>
2025-07-17 15:22:45 -0400

    target4

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ module "blog_autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = modules.blog_vpc.public_subnets
-  target_groups_arns   = module.blog_alb.target_groups[ex-instance].arn
+  alb_target_group_arns   = module.blog_alb.target_groups[ex-instance].arn
   security_groups     = [module.blog_sg.security_group_id]
 
   image_id      = data.aws_ami.app_ami.id
